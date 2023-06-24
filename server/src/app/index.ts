8ac64c25dcc02d6af10090660901398884fd36ab
Tautik Agrahari <tautikagrahari@gmail.com>
2023-06-24 23:27:23 +0530

    Twitter server backend setup done

---
+++ server/src/app/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import express from "express";
+
+export async function initServer() {
+  const app = express();
+  app.use(bodyParser.json());
+
+  const graphqlServer = new ApolloServer({
+    typeDefs: `
+        type Query {
+            sayHello : String
+        }
+    `,
+    resolvers: {
+      Query: {
+        sayHello: () => `Hello from graphql server`,
+      },
+    },
+  });
+
+  await graphqlServer.start();
+
+  app.use("/graphql", expressMiddleware(graphqlServer));
+
+  return app;
+}
